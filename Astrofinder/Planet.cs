2cbb980f7110f2ba6f8689ab33f78ef8d5eaf18d
Bezerra <pabezerra96@gmail.com>
2020-12-02 22:06:05 +0000

    Update the Comparison methods on Planet.cs to work with nullables

--- Astrofinder/Planet.cs
+++ Astrofinder/Planet.cs
@@ -1,53 +1,55 @@
+using System;
+
 namespace Astrofinder
 {
-    public struct Planet
+    public class Planet
     {
         public static Comparison<Planet> CompareByName = 
             delegate (Planet p1, Planet p2)
         {
-            return p1.Name.CompareTo(p2.Name);
+            return String.Compare(p1.Name, p2.Name);
         };
 
         public static Comparison<Planet> CompareByHostName = 
             delegate (Planet p1, Planet p2)
         {
-            return p1.Host.CompareTo(p2.Host);
+            return String.Compare(p1.Host, p2.Host);
         };
 
         public static Comparison<Planet> CompareByDiscMethod = 
             delegate (Planet p1, Planet p2)
         {
-            return p1?.DiscMethod?.CompareTo(p2?.DiscMethod);
+            return String.Compare(p1.DiscMethod, p2.DiscMethod);
         };
 
         public static Comparison<Planet> CompareByDiscYear = 
             delegate (Planet p1, Planet p2)
         {
-            return p1?.DiscYear?.CompareTo(p2?.DiscYear);
+            return p1?.DiscYear?.CompareTo(p2.DiscYear) ?? -1;
         };
 
         public static Comparison<Planet> CompareByOrbitalPeriod = 
             delegate (Planet p1, Planet p2)
         {
-            return p1?.OrbitalPeriod?.CompareTo(p2?.OrbitalPeriod);
+            return p1?.OrbitalPeriod?.CompareTo(p2.OrbitalPeriod) ?? -1;
         };
 
         public static Comparison<Planet> CompareByRadius = 
             delegate (Planet p1, Planet p2)
         {
-            return p1?.Radius?.CompareTo(p2?.Radius);
+            return p1?.Radius?.CompareTo(p2.Radius) ?? -1;
         };
 
         public static Comparison<Planet> CompareByMass = 
             delegate (Planet p1, Planet p2)
         {
-            return p1?.Mass?.CompareTo(p2?.Mass);
+            return p1?.Mass?.CompareTo(p2.Mass) ?? -1;
         };
 
         public static Comparison<Planet> CompareByTemperature = 
             delegate (Planet p1, Planet p2)
         {
-            return p1?.Temperature?.CompareTo(p2?.Temperature);
+            return p1?.Temperature?.CompareTo(p2.Temperature) ?? -1;
         };
 
 
@@ -58,14 +60,14 @@ namespace Astrofinder
         //discoverymethod 
         public string DiscMethod {get; set;}
         //disc_year 
-        public short DiscYear {get; set;}
+        public short? DiscYear {get; set;}
         //pl_orber
-        public short OrbitalPeriod {get;set;}
+        public short? OrbitalPeriod {get;set;}
         //pl_rade
-        public float Radius {get; set;}
+        public float? Radius {get; set;}
         //pl_masse
-        public float Mass {get; set;}
+        public float? Mass {get; set;}
         //pl_eqt
-        public float Temperature {get; set;}
+        public float? Temperature {get; set;}
     }
 }
