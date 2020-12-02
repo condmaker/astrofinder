ab77bfebd4486ca3e5c7b664b8767f9e8c608b2d
Marco Domingos <supersthotaku@gmail.com>
2020-12-02 19:32:16 +0000

    Add UI class and Unicode encoding on Program

--- Astrofinder/Program.cs
+++ Astrofinder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Astrofinder
 {
@@ -6,7 +7,8 @@ namespace Astrofinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Makes it so that the program supports unicode characters.
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
         }
     }
 }
