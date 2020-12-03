fbd1098b6645166760e6d9ad702de8a3308dfa84
Bezerra <pabezerra96@gmail.com>
2020-12-03 15:13:49 +0000

    Add Handler Class

---
+++ Astrofinder/Handler.cs
@@ -0,0 +1,41 @@
+namespace Astrofinder
+{
+    public class Handler
+    {
+        // receber filename de UI e invocar FileReader
+        void ReadFile(string path)
+        {
+
+        }
+
+        // Receber string de UI e atualizar query params
+        void UpdateParams(string filter)
+        {
+
+        }
+
+        // Enviar lista para ConsoleClient
+        List<Planet> SearchPlanets()
+        {
+
+        }
+
+        // Enviar lista para ConsoleClient
+        List<Star> SearchStars()
+        {
+
+        }
+
+        // Enviar planet / estrela para UI
+        Planet ViewPlanet(string name)
+        {
+
+        }
+
+        // Enviar planet / estrela para UI
+        Star ViewStar(string name)
+        {
+            
+        }
+    }
+}
